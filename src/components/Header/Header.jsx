0e382778cb0db71634f173c357af4653315c146d
URETTS <yurka.b2000@gmail.com>
2025-07-03 23:32:56 +0200

    header start

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,59 +1,73 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { logOut } from "../../redux/auth/operations.js";
 import css from "./Header.module.css";
+import { LogOut } from "lucide-react"; 
+import logo from "/logo/vite.svg";
 
 export default function Header() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const user = useSelector((state) => state.auth.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleLogout = async () => {
     await dispatch(logOut());
     navigate("/");
   };
 
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
-    <header>
-      <Link className={css.margin} to="/">
-        IMG
-      </Link>
-      {isLoggedIn ? (
-        <>
-          <Link className={css.margin} to="/">
-            RECIPES
-          </Link>
+    <header className={css.header}>
+      <div className={`container ${css.inner}`}>
+        <Link className={css.logo} to="/">
+          <img src={logo} alt="Logo" />
+          <span className={css.logoText}>Tasteorama</span>
+        </Link>
 
-          <Link className={css.margin} to="/profile/own">
-            PROFILE
-          </Link>
+        <button
+  className={css.burger}
+  onClick={toggleMenu}
+  aria-label="Toggle menu"
+>
+  {menuOpen ? (
+    <span className={css.closeIcon}>&times;</span>
+  ) : (
+    <>
+      <span></span>
+      <span></span>
+      <span></span>
+    </>
+  )}
+</button>
 
-          <Link className={css.margin} to="/add-recipe">
-            ADD RECIPE
-          </Link>
+        <nav className={`${css.nav} ${menuOpen ? css.open : ""}`}>
+          <Link to="/" className={css.link}>Recipes</Link>
 
-          <span className={css.margin}>{user.name}</span>
+          {isLoggedIn ? (
+            <>
+              <Link to="/profile/own" className={css.link}>My Profile</Link>
+              <Link to="/add-recipe" className={css.addBtn}>Add Recipe</Link>
 
-          <button className={css.margin} onClick={handleLogout}>
-            LOGOUT
-          </button>
-        </>
-      ) : (
-        <>
-          <Link className={css.margin} to="/">
-            RECIPES
-          </Link>
-
-          <Link className={css.margin} to="/auth/login">
-            LOGIN
-          </Link>
-
-          <Link className={css.margin} to="/auth/register">
-            REGISTER
-          </Link>
-        </>
-      )}
+              <div className={css.userBlock}>
+                <div className={css.avatar}>{user.name?.[0]}</div>
+                <span className={css.userName}>{user.name}</span>
+                <button onClick={handleLogout} className={css.logoutIcon} title="Logout">
+                  <LogOut size={18} />
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              <Link to="/auth/login" className={css.link}>Log in</Link>
+              <Link to="/auth/register" className={css.registerBtn}>Register</Link>
+            </>
+          )}
+        </nav>
+      </div>
     </header>
   );
 }
