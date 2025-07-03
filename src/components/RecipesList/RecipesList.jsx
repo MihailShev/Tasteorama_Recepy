03789ca5577576661aa6aac459f507024e7846d0
Panchenko Denis <Pandenisko@gmail.com>
2025-07-03 21:24:52 +0300

    recipeList created

--- src/components/RecipesList/RecipesList.jsx
+++ src/components/RecipesList/RecipesList.jsx
@@ -1,3 +1,14 @@
-export default function RecipesList() {
-  return <></>;
+import RecipeCard from "../RecipeCard/RecipeCard";
+import css from "./RecipesList.module.css";
+
+export default function RecipesList({ recipes }) {
+  return (
+    <ul className={css.list}>
+      {recipes.map((recipe) => (
+        <li key={recipe._id} className={css.item}>
+          <RecipeCard recipe={recipe} />
+        </li>
+      ))}
+    </ul>
+  );
 }
