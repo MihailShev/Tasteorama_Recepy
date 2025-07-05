7470e42e60c178d2455d18ab4a6d345ad64944d2
Neon <neonmed571@gmail.com>
2025-07-05 15:20:17 +0300

    finish page for adding recipes

--- src/pages/AddRecipePage/AddRecipePage.jsx
+++ src/pages/AddRecipePage/AddRecipePage.jsx
@@ -1,9 +1,34 @@
-import AddRecipeForm from "../../components/AddRecipeForm/AddRecipeForm.jsx";
+import axios from 'axios';
+import { useState, useMemo } from 'react';
+import AddRecipeForm from '../../components/AddRecipeForm/AddRecipeForm.jsx';
+import { Navigate } from 'react-router';
 
 export default function AddRecipePage() {
-  return (
-    <>
-      <AddRecipeForm />
-    </>
-  );
+  const [isError, setIsError] = useState(false);
+  const [categoryList, setCategoryList] = useState([]);
+  const [ingredientList, setIngredientList] = useState([]);
+
+  useMemo(() => {
+    try {
+      setIsError(false);
+      axios
+        .get('https://recepy-api.onrender.com/api/categories/all')
+        .then(info => setCategoryList(info.data.data));
+      axios
+        .get('https://recepy-api.onrender.com/api/ingredients')
+        .then(info => setIngredientList(info.data.data));
+    } catch {
+      setIsError(true);
+    }
+  }, []);
+
+  if (!isError) {
+    return (
+      <>
+        <AddRecipeForm categories={categoryList} ingredients={ingredientList} />
+      </>
+    );
+  } else {
+    return <Navigate to="/recipes" replace />;
+  }
 }
