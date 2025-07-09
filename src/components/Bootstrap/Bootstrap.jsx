064fdc5cc0436cce8d2815d7fbb259c8c1e7da02
Mihail <mykh.shev4enko@gmail.com>
2025-07-09 15:56:42 +0300

    fix_Bootstrap

--- src/components/Bootstrap/Bootstrap.jsx
+++ src/components/Bootstrap/Bootstrap.jsx
@@ -1,26 +1,13 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { api } from "../../redux";
-import { setCredentials } from "../../redux/auth/slice.js";
+import { useDispatch, useSelector } from "react-redux";
 import { refresh } from "../../redux/auth/operations.js";
 
 export default function Bootstrap({ children }) {
   const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
   useEffect(() => {
-    const token = localStorage.getItem("accessToken");
-    const user = localStorage.getItem("user");
-
-    if (token && user) {
-      api.defaults.headers.common.Authorization = `Bearer ${token}`;
-
-      dispatch(
-        setCredentials({
-          token: token,
-          user: JSON.parse(user),
-        })
-      );
-
+    if (isLoggedIn) {
       dispatch(refresh());
     }
   }, []);
